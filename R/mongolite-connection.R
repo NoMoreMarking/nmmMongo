09f309d6e0311a49c2745713ca3d706d12acbdfd
Chris Wheadon <chris.wheadon@gmail.com>
2018-05-10 14:37:36 +0100

    sales

--- R/mongolite-connection.R
+++ R/mongolite-connection.R
@@ -354,3 +354,19 @@ getScaling <- function(task,connStr){
   return(tasks)
 }
 
+#' Get sales data
+#'
+#' @param project The project id
+#' @param connStr A connection string.
+#' @return data frame product data
+#' @examples
+#' getSales('vqPwL2wWX8qc26nWs','mongodb://')
+#' @export
+getSales <- function(product,connStr){
+  basket <- mongolite::mongo('shop.basket2',url=connStr)
+  qryString <- paste0('{"product":"',product,'", "status":"purchased"}')
+  salesList <- basket$find(query = qryString,
+                         fields = '{"purchaseDate": true}')
+  sales <- jsonlite::flatten(salesList)
+  return(sales)
+}
