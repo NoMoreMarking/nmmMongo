d86b2ef3ec08e27a9f9161d23adea75bcbe453e9
Chris Wheadon <chris@nomoremarking.com>
2021-07-06 11:14:02 +0100

    updated file structure

--- R/mongolite-connection.R
+++ R/mongolite-connection.R
@@ -101,36 +101,6 @@ getProduct <- function(id, connStr){
   return(paste0('no product with id: ',id))
 }
 
-#' Get syllabus object from a syllabus name
-#'
-#' @param name The name for the syllabus
-#' @param connStr A connection string.
-#' @return A data frame with syllabus object
-#' @examples
-#' getSyllabusByName('SACS', 'mongodb://') will match all SACS syllabuses.
-#' @export
-getSyllabusByName <- function(name,connStr){
-  syllabusCollection <- mongolite::mongo(db='nmm-vegas-db',collection="syllabus",url=connStr)
-  qryString <- paste0('{"name":{"$regex":"',name,'","$options":"i"}}')
-  syllabusList <- syllabusCollection$find(query = qryString,fields = '{"name" : true, "acYear": true, "modCode":true, "yearGroup":true, "product":true}')
-  return(syllabusList)
-}
-
-#' Get syllabus object from a product name
-#'
-#' @param product The product for the syllabus
-#' @param connStr A connection string.
-#' @return A data frame with syllabus objects
-#' @examples
-#' getSyllabusByProduct('productid', 'mongodb://') will match all SACS syllabuses.
-#' @export
-getSyllabusByProduct <- function(product,connStr){
-  syllabusCollection <- mongolite::mongo(db='nmm-vegas-db',collection="syllabus",url=connStr)
-  qryString <- paste0('{"product":"',product,'"}')
-  syllabusList <- syllabusCollection$find(query = qryString,fields = '{"name" : true, "acYear": true, "modCode":true, "yearGroup":true, "product":true}')
-  return(syllabusList)
-}
-
 #' Get task ids, names and number of decisions completed for tasks with a given moderation code
 #'
 #' @param modCode The modCode for the tasks required
---
+++ R/syllabus.R
@@ -0,0 +1,29 @@
+#' Get syllabus object from a syllabus name
+#'
+#' @param name The name for the syllabus
+#' @param connStr A connection string.
+#' @return A data frame with syllabus object
+#' @examples
+#' getSyllabusByName('SACS', 'mongodb://') will match all SACS syllabuses.
+#' @export
+getSyllabusByName <- function(name,connStr){
+  syllabusCollection <- mongolite::mongo(db='nmm-vegas-db',collection="syllabus",url=connStr)
+  qryString <- paste0('{"name":{"$regex":"',name,'","$options":"i"}}')
+  syllabusList <- syllabusCollection$find(query = qryString,fields = '{"name" : true, "acYear": true, "modCode":true, "yearGroup":true, "product":true}')
+  return(syllabusList)
+}
+
+#' Get syllabus object from a product name
+#'
+#' @param product The product for the syllabus
+#' @param connStr A connection string.
+#' @return A data frame with syllabus objects
+#' @examples
+#' getSyllabusByProduct('productid', 'mongodb://') will match all SACS syllabuses.
+#' @export
+getSyllabusByProduct <- function(products,connStr){
+  syllabusCollection <- mongolite::mongo(db='nmm-vegas-db',collection="syllabus",url=connStr)
+  qryString <- paste0('{"product":"',product,'"}')
+  syllabusList <- syllabusCollection$find(query = qryString,fields = '{"name" : true, "acYear": true, "modCode":true, "yearGroup":true, "product":true}')
+  return(syllabusList)
+}
