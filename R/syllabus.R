2eb86684459301731897e95a48a70262763a4843
Chris Wheadon <chris@nomoremarking.com>
2022-03-15 11:13:38 +0000

    activeStart

--- R/syllabus.R
+++ R/syllabus.R
@@ -9,7 +9,7 @@
 getSyllabusByName <- function(name,connStr){
   syllabusCollection <- mongolite::mongo(db='nmm-vegas-db',collection="syllabus",url=connStr)
   qryString <- paste0('{"name":{"$regex":"',name,'","$options":"i"}}')
-  syllabusList <- syllabusCollection$find(query = qryString,fields = '{"name" : true, "acYear": true, "modCode":true, "yearGroup":true, "product":true, "writingAgeSet":true, "writingAge": true}')
+  syllabusList <- syllabusCollection$find(query = qryString,fields = '{"name" : true, "acYear": true, "modCode":true, "yearGroup":true, "product":true, "writingAgeSet":true, "writingAge": true,"activeStart":true}')
   return(syllabusList)
 }
 
@@ -24,6 +24,6 @@ getSyllabusByName <- function(name,connStr){
 getSyllabusByProduct <- function(product,connStr){
   syllabusCollection <- mongolite::mongo(db='nmm-vegas-db',collection="syllabus",url=connStr)
   qryString <- paste0('{"product":"',product,'"}')
-  syllabusList <- syllabusCollection$find(query = qryString,fields = '{"name" : true, "acYear": true, "modCode":true, "yearGroup":true, "product":true}')
+  syllabusList <- syllabusCollection$find(query = qryString,fields = '{"name" : true, "acYear": true, "modCode":true, "yearGroup":true, "product":true, "activeStart":true}')
   return(syllabusList)
 }
