210ade79d4097a4dc04c85c045613a49e1d40c24
Chris Wheadon <chris@nomoremarking.com>
2024-07-08 13:46:49 +0100

    scans pages

---
+++ R/images.R
@@ -0,0 +1,17 @@
+#' Get image paths
+#'
+#' @param task The id of the local task
+#' @param connStr A connection string
+#' @return A data frame with details from the scans.Pages collection
+#' @examples
+#' getImages('abc', 'mongodb://') 
+#' @export
+getImages <- function(task,connStr){
+  scansCollection <- mongolite::mongo(db='nmm-vegas-db',collection="scans.Pages",url=connStr)
+  qryString <- paste0('{"task":"',task,'"}')
+  imageList <- scansCollection$find(query = qryString,fields = '{"qrcode" : true, "url":true, "thumbnail0": true, "createdAt":true}')
+  if(nrow(imageList)==0){
+    cat('No images found for', task,'.\n')
+  }
+  return(imageList)
+}
