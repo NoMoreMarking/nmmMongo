2f606ad1024167c15d9da122e0bd7d99fb6aaed2
Chris Wheadon <chris@nomoremarking.com>
2025-06-04 10:27:08 +0100

    product by name

--- R/products.R
+++ R/products.R
@@ -10,3 +10,16 @@ getProduct <- function(id, connStr){
   if('productName' %in% names(productList)) return (productList)
   return(paste0('no product with id: ',id))
 }
+
+#' Get product id from a product name
+#'
+#' @param name The name of the product
+#' @param connStr A connection string
+#' @export
+getProductByName <- function(name, connStr){
+  productCollection <- mongolite::mongo(db='nmm-vegas-db',collection="products",url=connStr)
+  qryString <- paste0('{"productName":"',name,'"}')
+  productList <- productCollection$find(query=qryString, fields = '{"emailContent":false, "sPoints": false}',limit = 1)
+  if('productName' %in% names(productList)) return (productList)
+  return(paste0('no product with name: ',name))
+}
