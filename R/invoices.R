229fdf2b2bae0a84bbddf462b3a00ed791eb9d52
Chris Wheadon <chris@nomoremarking.com>
2025-06-11 12:30:41 +0100

    invoice by dfe

--- R/invoices.R
+++ R/invoices.R
@@ -16,3 +16,22 @@ getInvoiceByNumber <- function(invoice,connStr){
     cat('no invoice found for that number') 
   }
 }
+
+#' Get invoice details from dfe number
+#'
+#' @param dfe The dfe number
+#' @param connStr A connection string.
+#' @return A data frame with invoices
+#' @examples
+#' getInvoicesByDfe(123, 'mongodb://')
+#' @export
+getInvoicesByDfe <- function(dfe,connStr){
+  invoiceCollection <- mongolite::mongo(db='nmm-vegas-db',collection="invoices",url=connStr)
+  qryString <- paste0('{"dfe":',dfe,'}')
+  invoiceObject <- invoiceCollection$find(query = qryString)
+  if(nrow(invoiceObject)>0){
+    return(invoiceObject)
+  } else {
+    cat('no invoices found for ', dfe) 
+  }
+}
