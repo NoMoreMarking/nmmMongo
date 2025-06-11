2f4bb1c0fe8e22506f69cef9368fc0ada294aa64
Chris Wheadon <chris@nomoremarking.com>
2025-06-11 15:06:04 +0100

    create invoice

--- R/invoices.R
+++ R/invoices.R
@@ -35,3 +35,43 @@ getInvoicesByDfe <- function(dfe,connStr){
     cat('no invoices found for ', dfe) 
   }
 }
+
+
+#' Create a new invoice
+#'
+#' @param dfe dfe number
+#' @param product product id
+#' @param purchase purchase id
+#' @param no invoice number
+#' @param connStr A connection string.
+#' @return A string with confirmation
+#' @examples
+#' createInvoice(dfe, product, purchase, no, 'mongodb://')
+#' @export
+createInvoice <- function(dfe, product, purchase_id, no, connStr){
+  new_invoice_id <- as.character(uuid::UUIDgenerate())
+  created_at = format(lubridate::today(), "%Y-%m-%d")
+  expires_at = format(lubridate::today() + lubridate::days(30), "%Y-%m-%d")
+  # Connect to the 'invoices' collection using your existing connection string
+  invoices_collection <- mongolite::mongo(db='nmm-vegas-db',collection="invoices",url=connStr)
+  
+  # Create the document to insert
+  new_invoice <- tibble(
+    `_id` = new_invoice_id,
+    dfe = dfe,
+    product = product,
+    purchase = purchase_id,
+    no = no,
+    createdAt = created_at,
+    expiresAt = expires_at,
+    newUtilities = TRUE
+  )
+  
+  # Insert the document into the collection
+  result <- invoices_collection$insert(new_invoice)
+  row_string <- with(new_invoice, 
+                     sprintf("Invoice #%s for %s - Created: %s", 
+                             no, dfe, createdAt))
+  return(paste("Invoices created: ",result$nInserted, row_string))
+  
+}
